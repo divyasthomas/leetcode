39df484a5992ead5a6316398fa44733ab4db5f23
Divya Thomas <divyasthomas@gmail.com>
2024-05-31 19:07:03 -0500

    day1

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -17,23 +17,29 @@ class Solution:
 
 
 
+
 def main():
     solution = Solution()
 
     while True:
         try:
             uInput = input('Enter strings separated by a comma: ')
-            parts = uInput.split(',')
-            if parts:  # Ensure neither string is empty
-                    break
-            print("Invalid input. Please enter two strings separated by a comma.")
+            parts = [s.strip() for s in uInput.split(',')]
+            
+            if not (1 <= len(parts) <= 104):
+                print("Invalid input. Please enter between 1 and 10,000 strings.")
+                continue
+            
+            if any(len(s) > 100 or not s.islower() or not s.isalpha() for s in parts):
+                print("Invalid input. Ensure each string is at most 100 characters long, consists of lowercase English letters, and contains no punctuation or special characters.")
+                continue
+            
+            break
         except Exception as e:
-            print(f"Error: {e}. Please enter two strings separated by a comma.")
-
+            print(f"Error: {e}. Please enter the strings correctly.")
 
     res = solution.groupAnagrams(parts)
     print(res)
-
 
 if __name__ == "__main__":
     main()
